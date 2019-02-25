9502c141be9b3f7c3b8707cf5c26ac04ea5d62fe
Arun Kumar Kandakatla <s786409@dnata.com>
2019-02-25 22:55:41 +0400

    Initial code commit

---
+++ my-config-service/src/main/java/com/arun/myconfigservice/MyConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.arun.myconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MyConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyConfigServiceApplication.class, args);
+	}
+
+}
+
